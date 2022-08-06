bba3af3d34312dbfc7ae06e54d5df49987cfc23e
OLOWOOKEREISAAC <isaccolowookere1@gmail.com>
2022-08-06 05:45:31 +0100

    i added the basic class model for our products

--- priceCompare/Haggle/models.py
+++ priceCompare/Haggle/models.py
@@ -1,3 +1,12 @@
+from pickletools import read_stringnl_noescape
 from django.db import models
 
 # Create your models here.
+class productDetails(models.Model):
+    name = models.CharField(max_length= 50  , null= False)
+    imageLink = models.CharField(max_length= 50 , null= False)
+    ratings = models.CharField(max_length= 50 , null= False)
+    price = models.CharField(max_length= 50 , null= False)
+    merchantName = models.CharField(max_length=40 , null= False)
+    brand = models.CharField(max_length=40 , null= False )
+    category = models.CharField(max_length= 50 , null= False)
