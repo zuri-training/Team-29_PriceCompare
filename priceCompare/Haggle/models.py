cc792e47a6064bc141135e363649a0a5007cee7b
Jude Oyedele <judeokennywise@gmail.com>
2022-08-06 10:58:06 +0100

    Added comment model, form

--- priceCompare/Haggle/models.py
+++ priceCompare/Haggle/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class productDetails(models.Model):
@@ -9,3 +10,14 @@ class productDetails(models.Model):
     merchantName = models.CharField(max_length=40 , null= False)
     brand = models.CharField(max_length=40 , null= False )
     category = models.CharField(max_length= 50 , null= False)
+
+
+
+class ProductComment(models.Model):
+    author = models.ForeignKey(User, editable=False, on_delete=models.CASCADE)
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    product = models.ForeignKey('productDetails', on_delete=models.CASCADE)
+
+    def __str__(self):
+        return 'Comment {} by {} '.format(self.body, self.author)
