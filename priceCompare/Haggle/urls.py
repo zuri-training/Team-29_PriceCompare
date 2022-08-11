872d557074a7691a9588a7981f3cce57997ed768
Abdulkareem Olawale <olawaleabiola741@gmail.com>
2022-08-12 09:50:33 +1300

    Template for Landing Page Privacy page

--- priceCompare/Haggle/urls.py
+++ priceCompare/Haggle/urls.py
@@ -1,5 +1,10 @@
 from django.urls import path
+from . import views
 
 urlpatterns = [
-    
+    path('', views.home, name='Home'),
+    path('about/', views.about, name='AboutPage'),
+    path('contact/', views.contact, name='Contact'),
+    path('profile/', views.profileCard, name='ProfileCard'),
+    path('privacy/', views.privacy, name='Privacy'),
 ]
--- priceCompare/Haggle/views.py
+++ priceCompare/Haggle/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def home(request):
+    return render(request, 'index.html')
+
+def privacy(request):
+    return render(request, 'privacy.html')
+
+def about (request):
+    return render(request, 'about.html')
+
+def contact(request):
+    return render(request, 'contact.html')
+
+def profileCard(request):
+    return render(request, 'profilecard.html')
