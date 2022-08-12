6d89937c41f1e7056e15820aeafa3a3e0847dce2
unknown <onyishiprecious73@gmail.com>
2022-08-12 19:37:10 +0100

    Implemented the template, url and views of the profile card page

--- priceCompare/Haggle/urls.py
+++ priceCompare/Haggle/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
 from .views import faq
+from .views import profilecard
 
 urlpatterns = [
-    path('faq/', faq, name = 'faq' )
+    path('faq/', faq, name = 'faq' ),
+    path('profilecard/', profilecard, name = 'profilecard' )
     
 ]
+
+    
+
--- priceCompare/Haggle/views.py
+++ priceCompare/Haggle/views.py
@@ -4,3 +4,5 @@ from django.shortcuts import render
 
 def faq(request):
     return render(request, 'faq.html')
+def profilecard(request):
+    return render(request, 'profilecard.html')
