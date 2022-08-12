f7888e4d792f63291db99fd6ec12eb478537f2cf
Kehinde Ojewale <kennyoje1@gmail.com>
2022-08-12 16:02:40 +0100

    Price compare view

--- priceCompare/Haggle/urls.py
+++ priceCompare/Haggle/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import faq, Home, pricecompare, SearchResultView
+from .views import faq, Home, PriceCompareView, SearchResultView
 
 urlpatterns = [
     path('faq/', faq, name = 'faq' ),
     path('home', Home, name='home'),
-    path('<int:pk>/', pricecompare, name='compare'),
+    path('<int:pk>/', PriceCompareView.as_view(), name='compare'),
     path('', SearchResultView.as_view(), name= 'search')
 
 ]
--- priceCompare/Haggle/views.py
+++ priceCompare/Haggle/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from django.views.generic.detail import DetailView
+from django.views.generic.edit import FormMixin
 from .models import productDetails, ProductComment
 from .forms import CommentForm
+from django.urls import reverse
 from django.views.generic import ListView
 import random
 # Create your views here.
@@ -34,24 +38,79 @@ class SearchResultView(ListView):
 
         return results
 
+# Create your views here.
 
+# class ProductListView(ListView):
+#     model = productDetails
+#     paginate_by= 50
 
-def pricecompare(request, pk):
-    product = productDetails.objects.get(pk=pk)
-    form = CommentForm()
+#     results= model.objects.filter(brand=brand).filter(category=category)
 
-    if request.method == 'POST':
-        form = CommentForm(request.POST)
+#     # def get_context_data(self, **kwargs):
+#     #     context=
+#     #     return context
+
+class PriceCompareView(FormMixin, DetailView):
+    model = productDetails
+    template_name = 'comparison.html'
+    form_class = CommentForm
+
+    def get_success_url(self):
+        return reverse('Haggle:product', kwargs={'pk': self.object.pk})
+
+    def product_comparison_fetch(self):
+        import re
+        brand= self.object.brand
+        reduced_name=re.search(f'{brand}(.+)',self.object.name).group()
+
+        #CASES FOR QUERY VALUE
+        case_1=' '.join(reduced_name.split()[0:4])
+        case_2=' '.join(reduced_name.split()[0:3])
+        case_3=' '.join(reduced_name.split()[0:2])
+        case_4= brand + ' ' + reduced_name.split()[2]
+
+        if productDetails.objects.filter(name__icontains=case_1).exists():
+            query_value=case_1
+        elif productDetails.objects.filter(name__icontains=case_2).exists():
+            query_value=case_2
+        elif productDetails.objects.filter(name__icontains=case_3).exists():
+            query_value=case_3
+        elif productDetails.objects.filter(name__icontains=case_4).exists():
+            query_value= case_4
+        else:
+            query_value=brand
+
+        slot=productDetails.objects.filter(merchantName='Slot').filter(name__icontains=query_value).order_by('price')
+        jumia=productDetails.objects.filter(merchantName='Jumia').filter(name__icontains=query_value).order_by('price')
+        konga=productDetails.objects.filter(merchantName='Konga').filter(name__icontains=query_value).order_by('price')
+        
+        result_list=[slot,jumia,konga]
+        return_list=[]
+            
+        for i in result_list:
+            if i.exists():
+                return_list.append(i[0])
+        
+        return return_list
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['product'] = self.object
+        context['comparisons'] = self.product_comparison_fetch()
+        context['comments'] = ProductComment.objects.filter(product=self.object.id).order_by('created_on')
+        context['form'] = self.get_form()
+        return context
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        form = self.get_form()
         if form.is_valid():
-            instance = form.save(commit=False)
-            instance.author = request.author
-            instance.save()
-    comment = ProductComment.objects.filter(product=product)
-    context= {
-        'product': product,
-        'comment': comment,
-        'form': form
-    }
-    return render(request, 'comparison.html', context)
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
 
 
