f4d5e2cbff048f606fedda371cd7794d31fdc04a
Kehinde Ojewale <kennyoje1@gmail.com>
2022-08-13 18:45:28 +0100

    Functional comparison page view

--- priceCompare/Haggle/views.py
+++ priceCompare/Haggle/views.py
@@ -1,4 +1,5 @@
 
+from itertools import product
 from unicodedata import name
 from django.shortcuts import get_object_or_404, render
 from django.views.generic.list import ListView
@@ -120,17 +121,6 @@ class SearchResultView(ListView):
 
     
 
-# Create your views here.
-
-# class ProductListView(ListView):
-#     model = productDetails
-#     paginate_by= 50
-
-#     results= model.objects.filter(brand=brand).filter(category=category)
-
-#     # def get_context_data(self, **kwargs):
-#     #     context=
-#     #     return context
 
 class PriceCompareView(FormMixin, DetailView):
     model = productDetails
@@ -140,9 +130,13 @@ class PriceCompareView(FormMixin, DetailView):
     def get_success_url(self):
         return reverse('Haggle:product', kwargs={'pk': self.object.pk})
 
+    def get_product(self, *args, **kwargs):
+        return get_object_or_404(productDetails,id=self.kwargs['pk'])
+    
     def product_comparison_fetch(self):
         import re
-        brand= self.object.brand
+        product=self.get_product()
+        brand= product.brand
         reduced_name=re.search(f'{brand}(.+)',self.object.name).group()
 
         #CASES FOR QUERY VALUE
@@ -177,9 +171,10 @@ class PriceCompareView(FormMixin, DetailView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['product'] = self.object
+        product=self.get_product()
+        context['product'] = product
         context['comparisons'] = self.product_comparison_fetch()
-        context['comments'] = ProductComment.objects.filter(product=self.object.id).order_by('created_on')
+        context['comments'] = ProductComment.objects.filter(product=product.id).order_by('created_on')
         context['form'] = self.get_form()
         return context
 
